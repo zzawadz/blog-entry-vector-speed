4cbd2550c4c331c45cc10a218cf7f3920ff68c22
Zygmunt Zawadzki <zawadzkizygmunt@gmail.com>
2017-05-01 21:51:13 +0200

    Add ignores. Remove versions.

--- buildAllVersions.sh
+++ buildAllVersions.sh
@@ -1,4 +1,4 @@
-./versions.sh
+versions=( 3.3.3 3.4.0 )
 for i in "${versions[@]}"
 do
   ./installR.sh $i
--- buildTimeData.sh
+++ buildTimeData.sh
@@ -1,4 +1,4 @@
-./versions.sh
+versions=( 3.3.3 3.4.0 )
 for i in "${versions[@]}"
 do
   ./r-builds/R-$i/bin/R -f code.R --args $i
